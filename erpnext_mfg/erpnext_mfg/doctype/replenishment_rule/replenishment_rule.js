1b8d8044c71b4db949395c897f8535250798d4b5
Ivan Ray Altomera <irayspacii@gmail.com>
2021-06-08 19:09:09 +0800

    feat(doctype): added `Replenishment Rule`

---
+++ erpnext_mfg/erpnext_mfg/doctype/replenishment_rule/replenishment_rule.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Bai Web and Mobile Lab and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Replenishment Rule', {
+	// refresh: function(frm) {
+
+	// }
+});
