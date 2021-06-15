a8ae72eaa467ddb960346fe5b44194b2d1a84017
Ivan Ray Altomera <irayspacii@gmail.com>
2021-06-15 17:34:16 +0800

    feat(replenishment_rule): throw error has existing item rule

--- erpnext_mfg/erpnext_mfg/doctype/replenishment_rule/replenishment_rule.py
+++ erpnext_mfg/erpnext_mfg/doctype/replenishment_rule/replenishment_rule.py
@@ -3,8 +3,26 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
+
 class ReplenishmentRule(Document):
-	pass
+    def validate(self):
+        _has_existing_item_rule(self.name, self.item)
+
+
+def _has_existing_item_rule(name, item):
+    rules = frappe.get_all(
+        "Replenishment Rule",
+        filters={"name": ["!=", name], "item": item},
+    )
+    if rules:
+        frappe.throw(
+            _(
+                "Unable to create a Replenishment Rule. Please remove existing rule of item {}.".format(
+                    item
+                )
+            )
+        )
