3bfcda6ac99ae204cca60588da2184272b260f50
Ivan Ray Altomera <irayspacii@gmail.com>
2021-06-15 18:49:25 +0800

    fix(replenishment_auto_order): use order item

--- erpnext_mfg/events/replenish_auto_order.py
+++ erpnext_mfg/events/replenish_auto_order.py
@@ -1,60 +1,18 @@
 import frappe
 from frappe import _
+from erpnext_mfg.api.replenishment import order_item
 
 
 # bench execute erpnext_mfg.events.replenish_auto_order.replenish_auto_order
 def replenish_auto_order():
     replenishment_items = frappe.get_all(
-        "Replenishment Item",
-        filters={"is_auto_order": 1},
-        fields=["item", "order_qty", "max_qty", "supplier"],
+        "Replenishment Rule",
+        filters={"disabled": 0},
+        fields=["item", "warehouse", "supplier"],
     )
-
-    warehouse = frappe.db.get_single_value("Replenishment", "warehouse")
     for item in replenishment_items:
-        projected_qty = get_projected_qty(
+        order_item(
             item.get("item"),
-            warehouse,
+            item.get("warehouse"),
+            item.get("supplier"),
         )
-        order_with_projected_qty = projected_qty + item.get("order_qty")
-        if item.get("max_qty") >= order_with_projected_qty:
-            create_po_from_item(
-                item.get("item"),
-                item.get("order_qty"),
-                warehouse,
-                item.get("supplier"),
-            )
-
-
-def create_po_from_item(item, qty, warehouse, supplier=None):
-    if not supplier:
-        supplier = frappe.db.get_single_value("Replenishment", "supplier")
-
-    po = frappe.new_doc("Purchase Order")
-    po.supplier = supplier
-    po.set_warehouse = warehouse
-
-    po.append("items", {
-        "item_code": item,
-        "qty": qty
-    })
-
-    po.set_missing_values()
-    po.schedule_date = po.transaction_date
-
-    po.save()
-    po.submit()
-
-
-def get_projected_qty(item, warehouse):
-    bin_detail = frappe.get_all(
-        "Bin",
-        filters={
-            "item_code": item,
-            "warehouse": warehouse,
-        },
-        fields=["projected_qty"]
-    )
-    if not bin_detail:
-        frappe.throw(_("Please the selected item and its warehouse. It should match from where it is requested."))
-    return bin_detail[0].get("projected_qty") if bin_detail else None
